da1a8a35a42d0dc6c4307e29fe7e2253f86ead5a
hota1024 <hotalog@hota1024.com>
2020-07-14 14:27:18 +0900

    🏷️ Add postTodo action

--- src/app/controllers/api.controller.ts
+++ src/app/controllers/api.controller.ts
@@ -1,4 +1,4 @@
-import { Context, Get, HttpResponseOK } from '@foal/core'
+import { Context, Get, HttpResponseOK, Post, Delete, HttpResponseNotFound, HttpResponseNoContent } from '@foal/core'
 import { getRepository } from 'typeorm'
 import { Todo } from '../entities'
 
@@ -13,5 +13,15 @@ export class ApiController {
     const todos = await getRepository(Todo).find()
 
     return new HttpResponseOK(todos)
+  }
+
+  @Post('/todos')
+  async postTodo(ctx: Context) {
+    const todo = new Todo()
+    todo.text = ctx.request.body.text
+
+    await getRepository(Todo).save(todo)
+
+    return new HttpResponseOK(todo)
   }
 }
