526c6fe4b1d6e0e7a81c998089f7f7717c2cc21d
hota1024 <hotalog@hota1024.com>
2020-07-14 13:34:47 +0900

    🏷️ Add Todo entity

---
+++ src/app/entities/todo.entity.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id: number
+}
