c29e769c9307cd6fc6b403b45dc0fc3424ea0aee
hota1024 <hotalog@hota1024.com>
2020-07-14 14:04:58 +0900

    🏷️ Add getTodos action

--- src/app/controllers/api.controller.ts
+++ src/app/controllers/api.controller.ts
@@ -1,8 +1,17 @@
 import { Context, Get, HttpResponseOK } from '@foal/core'
+import { getRepository } from 'typeorm'
+import { Todo } from '../entities'
 
 export class ApiController {
   @Get('/')
   index(ctx: Context) {
     return new HttpResponseOK('Hello world!')
+  }
+
+  @Get('/todos')
+  async getTodos() {
+    const todos = await getRepository(Todo).find()
+
+    return new HttpResponseOK(todos)
   }
 }
