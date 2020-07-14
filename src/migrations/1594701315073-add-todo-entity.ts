b218d15a25aa91d1fc77ad42f7ed83be85511bad
hota1024 <hotalog@hota1024.com>
2020-07-14 13:37:48 +0900

    🚨 Fix linters error

--- src/migrations/1594701315073-add-todo-entity.ts
+++ src/migrations/1594701315073-add-todo-entity.ts
@@ -1,16 +1,21 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class addTodoEntity1594701315073 implements MigrationInterface {
-    name = 'addTodoEntity1594701315073'
+  name = 'addTodoEntity1594701315073'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)`, undefined);
-        await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)`, undefined);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)`,
+      undefined
+    )
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)`,
+      undefined
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`, undefined);
-        await queryRunner.query(`DROP TABLE "todo"`, undefined);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`, undefined)
+    await queryRunner.query(`DROP TABLE "todo"`, undefined)
+  }
 }
